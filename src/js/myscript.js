505eb2cc05f0cfa608af1be5051f526e1f9553d6
Sergei945 <94513147+Sergei945@users.noreply.github.com>
2022-05-04 21:00:08 +0300

    Update myscript.js

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,134 +1,3 @@
-// let objectArraysKeys = {
-//     arrTypeKeys: Object.keys(calculationSite.typeSite),
-//     arrDesignKeys: Object.keys(calculationSite.design),
-//     arrAdaptiveKeys: Object.keys(calculationSite.adaptive)
-// };
-
-// let price = {
-//     clientType: '',
-//     clientDesign: '',
-//     clientAdaptive: ''
-// };
-// let finalPrice = 0;
-// while (true) {
-//     calculationPrice();
-//     let questionClient = confirm(`Подтвердите ваш заказ
-//     Тип сайт ${price.clientType}
-//     Дизайн ${price.clientDesign}
-//     Адаптив ${price.clientAdaptive}
-//     Общая стоимость проекта ${finalPrice}`);
-//     if (questionClient) {
-//         break;
-//     } else {
-//         alert('Давайте попробуем снова');
-//         continue;
-//     }
-// }
-
-// function calculationPrice() {
-//     while (true) {
-//         type = prompt(
-//             `Какой тип сайта вы желаете?
-//             1. ${objectArraysKeys.arrTypeKeys[0]}
-//             2. ${objectArraysKeys.arrTypeKeys[1]}
-//             3. ${objectArraysKeys.arrTypeKeys[2]}
-//             4. ${objectArraysKeys.arrTypeKeys[3]}
-//                                             Введит название или номер`,
-//             'Визитка'
-//         );
-//         if (type <= objectArraysKeys.arrTypeKeys.length && type >= 1 && type.Object != String) {
-//             finalPrice += Number(calculationSite.typeSite[objectArraysKeys.arrTypeKeys[type - 1]]);
-//             price.clientType = objectArraysKeys.arrTypeKeys[type];
-//             alert(`Вы выбрали тип ${objectArraysKeys.arrTypeKeys[type]}
-//                     Стоимость данной услуги составляет: ${calculationSite.typeSite[
-//                         objectArraysKeys.arrTypeKeys[type - 1]
-//                     ]}
-//                     Перейдём к выбору дизайна`);
-//             alert(calculationSite.typeSite[objectArraysKeys.arrTypeKeys[type - 1]]);
-//         } else if (type.toLowerCase() in calculationSite.typeSite) {
-//             finalPrice += calculationSite.typeSite[type.toLowerCase()];
-//             price.clientType = type.toLowerCase();
-//             alert(`Вы выбрали тип ${type.toLowerCase()}
-//                     Стоимость данной услуги составляет: ${calculationSite.typeSite[type.toLowerCase()]}
-//                     Перейдём к выбору дизайна`);
-//         } else {
-//             alert('Попробуйте ввести порядковый номер или значение из списка');
-//             continue;
-//         }
-//         break;
-//     }
-
-//     while (true) {
-//         design = prompt(
-//             `Какой дизайн сайта вы желаете?
-//                 1. ${objectArraysKeys.arrDesignKeys[0]}
-//                 2. ${objectArraysKeys.arrDesignKeys[1]}
-//                 3. ${objectArraysKeys.arrDesignKeys[2]}
-//                 4. ${objectArraysKeys.arrDesignKeys[3]}
-//                                                 Введит название или номер`,
-//             'figma'
-//         );
-//         if (design <= objectArraysKeys.arrDesignKeys.length && design >= 1 && design.Object != String) {
-//             finalPrice += Number(calculationSite.design[objectArraysKeys.arrDesignKeys[design - 1]]);
-//             price.clientDesign = objectArraysKeys.arrDesignKeys[design];
-//             alert(`Вы выбрали дизайн ${objectArraysKeys.arrDesignKeys[design]}
-//                         Стоимость данной услуги составляет: ${calculationSite.design[
-//                             objectArraysKeys.arrDesignKeys[design - 1]
-//                         ]}
-//                         На данный момент общая сумма проекта: ${finalPrice}
-//                         П1ерейдём к выбору адаптива`);
-//             console.log(typeof calculationSite.design[objectArraysKeys.arrDesignKeys[design - 1]]);
-//         } else if (design.toLowerCase() in calculationSite.design) {
-//             1;
-//             finalPrice += calculationSite.design[design.toLowerCase()];
-//             price.clientDesign = design.toLowerCase();
-//             alert(`Вы выбрали дизайн ${design.toLowerCase()}
-//                         Стоимость данной услуги составляет: ${calculationSite.design[design.toLowerCase()]}
-//                         На данный момент общая сумма проекта: ${finalPrice}
-//                         Перейдём к выбору адаптива`);
-//         } else {
-//             alert('Попробуйте ввести порядковый номер или значение из списка');
-//             continue;
-//         }
-
-//         break;
-//     }
-
-//     while (true) {
-//         adaptive = prompt(
-//             `Какой адаптив сайта вы желаете?
-//             1. ${objectArraysKeys.arrAdaptiveKeys[0]}
-//             2. ${objectArraysKeys.arrAdaptiveKeys[1]}
-//             3. ${objectArraysKeys.arrAdaptiveKeys[2]}
-//             4. ${objectArraysKeys.arrAdaptiveKeys[3]}
-//                                             Введит название или номер`,
-//             'с адаптивом'
-//         );
-//         if (adaptive <= objectArraysKeys.arrAdaptiveKeys.length && adaptive >= 1 && adaptive.Object != String) {
-//             finalPrice += calculationSite.adaptive[objectArraysKeys.arrAdaptiveKeys[adaptive - 1]];
-//             price.clientAdaptive = objectArraysKeys.arrAdaptiveKeys[adaptive];
-//             alert(`Вы выбрали адаптив ${objectArraysKeys.arrAdaptiveKeys[adaptive]}
-//                     Стоимость данной услуги составляет: ${calculationSite.adaptive[
-//                         objectArraysKeys.arrAdaptiveKeys[adaptive - 1]
-//                     ]}
-//                     На данный момент общая сумма проекта: ${finalPrice}
-//                     Перейдём к итогам`);
-//         } else if (adaptive.toLowerCase() in calculationSite.adaptive) {
-//             finalPrice += calculationSite.adaptive[adaptive.toLowerCase()];
-//             price.clientAdaptive = adaptive.toLowerCase();
-//             alert(`Вы выбрали адаптива ${adaptive.toLowerCase()}
-//                     Стоимость данной услуги составляет: ${calculationSite.adaptive[adaptive.toLowerCase()]}
-//                     Общая сумма проекта: ${finalPrice}
-//                     Перейдём к итогам`);
-//         } else {
-//             alert('Попробуйте ввести порядковый номер или значение из списка');
-//             continue;
-//         }
-
-//         break;
-//     }
-// }
-
 $(document).ready(function() {
     new WOW().init();
     // build scenes
@@ -209,7 +78,7 @@ $(document).ready(function() {
         personal: 50000
     };
     let adaptive = {
-        notAdaptive: 0,
+        notAdaptive: 10000,
         withAdaptive: 15000
     };
 
@@ -218,24 +87,48 @@ $(document).ready(function() {
         clientDesign: 0,
         clientAdaptive: 0
     };
-    let finalPrice;
+    let finalPrice = 0;
     $('#list').on('change', function() {
-        finalPrice = 0;
         price.clientType = objactType[$(this).val()];
-        finalPrice += price.clientType;
+
+        if (
+            !price.clientDesign == 0 &&
+            !price.clientAdaptive == 0 &&
+            !price.clientAdaptive == 0 &&
+            !price.clientDesign == 0
+        ) {
+            finalPrice = price.clientDesign + price.clientAdaptive + price.clientType;
+            console.log(finalPrice);
+        } else {
+            finalPrice = 0;
+            finalPrice += price.clientType;
+        }
         $('.end-price').text(finalPrice);
     });
 
     $('#list2').on('change', function() {
-        finalPrice = price.clientType;
         price.clientDesign = design[$(this).val()];
-        finalPrice += price.clientDesign;
+        if (
+            !price.clientType == 0 &&
+            !price.clientAdaptive == 0 &&
+            !price.clientAdaptive == 0 &&
+            !price.clientType == 0
+        ) {
+            finalPrice = price.clientDesign + price.clientAdaptive + price.clientType;
+        } else {
+            finalPrice = price.clientType;
+            finalPrice += price.clientDesign;
+        }
         $('.end-price').text(finalPrice);
     });
     $('#list3').on('change', function() {
-        finalPrice = price.clientType + price.clientDesign;
         price.clientAdaptive = adaptive[$(this).val()];
-        finalPrice += price.clientAdaptive;
+        if (!price.clientType == 0 && !price.clientDesign == 0 && !price.clientDesign == 0 && !price.clientType == 0) {
+            finalPrice = price.clientDesign + price.clientAdaptive + price.clientType;
+        } else {
+            finalPrice = price.clientType + price.clientDesign;
+            finalPrice += price.clientAdaptive;
+        }
         $('.end-price').text(finalPrice);
     });
 });
